b2c67b9c14cc4cb85d4b491fac2af113f1fd963c
Sung Park <spark146@jhu.edu>
2016-12-21 22:33:19 +0900

    removed push

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -20,13 +20,6 @@ import Colors from '../constants/Colors';
 import registerForPushNotificationsAsync from '../api/registerForPushNotificationsAsync';
 
 export default class RootNavigation extends React.Component {
-  componentDidMount() {
-    this._notificationSubscription = this._registerForPushNotifications();
-  }
-
-  componentWillUnmount() {
-    this._notificationSubscription && this._notificationSubscription.remove();
-  }
 
   render() {
     return (
@@ -69,23 +62,7 @@ export default class RootNavigation extends React.Component {
     );
   }
 
-  _registerForPushNotifications() {
-    // Send our push token over to our backend so we can receive notifications
-    // You can comment the following line out if you want to stop receiving
-    // a notification every time you open the app. Check out the source
-    // for this function in api/registerForPushNotificationsAsync.js
-    registerForPushNotificationsAsync();
 
-    // Watch for incoming notifications
-    this._notificationSubscription = Notifications.addListener(this._handleNotification);
-  }
-
-  _handleNotification = ({origin, data}) => {
-    this.props.navigator.showLocalAlert(
-      `Push notification ${origin} with data: ${JSON.stringify(data)}`,
-      Alerts.notice
-    );
-  }
 }
 
 const styles = StyleSheet.create({
